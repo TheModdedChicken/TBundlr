74939256e62d00afb4d371d9b28260cebb4d69e4
Logan Shaw <loganshaw1744@gmail.com>
2021-10-09 19:31:02 +0800

    Added url formatter and changed CDN

--- src/bundler/TBundler.js
+++ src/bundler/TBundler.js
@@ -1,6 +1,5 @@
 class TBundler {
-  CDN_JS = 'https://combinatronics.com';
-  CDN_TEXT = 'https://raw.githubusercontent.com';
+  CDN = 'https://cdn.jsdelivr.net';
   programs = {};
 
   constructor () {}
@@ -27,16 +26,17 @@ class TBundler {
    *  cache: boolean
    * }} options
    */
-  fetchProgram (repo, options) {
-    getJSON(this.CDN_TEXT + repo + '/tb-config.json', (err, data) => {
+  fetchProgram (url, options) {
+    getJSON(formatCdnUrl(url) + '/tb-config.json', (err, data) => {
       if (err) throw err;
 
-      const id = this.programs[data.id] ? data.id + `_${new Date().getTime()}` : data.id;
+      var program = data;
+      const id = this.programs[program.id] ? program.id + `_${new Date().getTime()}` : program.id;
 
-      if (options.run) this.run(data);
+      if (options.run) this.run(program);
       if (options.cache === null || options.cache === true) {
-        delete data.id;
-        this.programs[id] = data;
+        delete program.id;
+        this.programs[id] = program;
       } // Program cache check should ALWAYS be the last step unless another step requires it to be cached first
     })
   }
@@ -51,11 +51,41 @@ class TBundler {
 /* Functions */
 
 /**
+ * 
+ * @param {string} url 
+ */
+function formatCdnUrl(url) {
+  var urlParts = url.replace(/(https:\/\/)|(http:\/\/)/gi, '').replace(/\/+|\\+/g, '/').split('/');
+  var urlOut = this.CDN;
+  var foundBranch = false;
+  var foundQuery = false;
+
+  for (var i = 0; i < urlParts; i++) {
+    const part = urlParts[i];
+
+    if (part.match(/(raw.githubusercontent.com)|(github.com)/g)) urlOut += 'gh'
+    else if (i === 3 && part !== 'blob' && part !== 'blame') {
+      urlOut += '@' + part;
+      foundBranch = true;
+    }
+    else if (i === 4 && !foundBranch) {
+      urlOut += '@' + part;
+      foundBranch = true;
+    }
+    else if (foundQuery || part.startsWith('?') || part.startsWith('#')) {
+      urlOut += part
+      foundQuery = true;
+    }
+    else urlOut += '/' + part;
+  }
+}
+
+/**
  * Yoinked from https://stackoverflow.com/a/35970894/14618276 on Oct 9 2021
  * @param {string} url 
  * @param {(err: Error, data: object)} callback
  */
-function getJSON (url, callback) {
+function getJSON(url, callback) {
   fetch(url).then(res => res.json()).then(data => {
     callback(null, data)
   }).catch(err => callback(err, null))
