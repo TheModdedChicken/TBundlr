48a34b7b227749536a08c644f9ebd6ee7c6f2ae6
TheModdedChicken <hi@loganshaw.org>
2024-04-22 11:56:47 +0800

    Turborepo

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,12 @@
+export default {
+  logo: <span>TBundlr</span>,
+  project: {
+    link: 'https://github.com/TheModdedChicken/TBundlr',
+  },
+  docsRepositoryBase: 'https://github.com/TheModdedChicken/TBundlr/tree/main/docs',
+  feedback: {
+    content: 'Question? Create an issue →',
+    labels: 'feedback'
+  },
+  // ...
+}
